0eac010ca008a4d7e0598d3198b203c2b64d9813
pradiep kummaar <110171321+pky1341@users.noreply.github.com>
2023-07-19 05:02:58 +0000

    commit

---
+++ js_variable/two.js
@@ -0,0 +1,10 @@
+// How to Use JavaScript Variables in Functional Scope
+
+function pky() {
+    var pk="pky";
+    let age=20;
+    const name='yadav ji';
+    console.log(age);
+}
+pky();
+// console.log();
