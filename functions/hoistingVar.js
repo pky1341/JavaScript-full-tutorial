ba088d6dc157230b06b09d3388317aa4c5e7e61c
pradiep kummaar <pky463775@gmail.com>
2024-01-07 20:51:27 +0530

    hi

---
+++ functions/hoistingVar.js
@@ -0,0 +1,2 @@
+console.log(object);
+let object='hello';
---
+++ functions/hositing_01.js
@@ -0,0 +1,8 @@
+printHello();
+function printHello() {
+    printDillion();
+    console.log('o hi how r u'); 
+    function printDillion() {
+        console.log(`o hi printDillion`);
+    }
+}
